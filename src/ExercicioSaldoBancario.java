5da8fda0976e158bcd8af10edb292bddd2d5c6fc
Onlyveira <davileandro46@gmail.com>
2023-08-28 22:24:43 -0300

    refatoração

--- src/ExercicioSaldoBancario.java
+++ src/ExercicioSaldoBancario.java
@@ -1,23 +1,53 @@
+import java.util.Scanner;
+
 public class ExercicioSaldoBancario {
     public static void main(String[] args) {
 
         String nome = "Leandro";
-        boolean contaCorrente = true;
+        String tipoDeConta = "Conta corrente";
         double saldoInicial = 2500.99;
-        String tipoConta;
+        int tentativasSenha = 0;
+        Scanner respostaCliente = new Scanner(System.in);
 
-        if(contaCorrente == true ) {
-            tipoConta = "Conta Corrente";
-        } else {
-            tipoConta = "Conta Poupança";
+
+        int senhaCliente = respostaCliente.nextInt();
+
+        while (senhaCliente != 1234 && tentativasSenha < 3) {
+            System.out.println("Insira a senha: ");
+            senhaCliente = respostaCliente.nextInt();
+            tentativasSenha++;
+
+            if (senhaCliente != 1234) {
+                int tentativasRestantes = 3 - tentativasSenha;
+                if (tentativasRestantes > 1) {
+                    System.out.println("Você tem " + tentativasRestantes + " tentativas restantes antes do bloqueio da conta.");
+                } else if (tentativasRestantes == 1) {
+                    System.out.println("Você tem uma tentativa restante antes do bloqueio da conta.");
+                }
+            }
+
+            if(senhaCliente == 1234) {
+                System.out.println("****************************");
+                System.out.println("\nDados Iniciais do Cliente:");
+                System.out.println("Nome: " + nome);
+                System.out.println(tipoDeConta);
+                System.out.println("Saldo Inicial: " + saldoInicial);
+                System.out.println("\n****************************");
+
+
+            } else if  (tentativasSenha >= 3) {
+                System.out.println("Você excedeu o número de tentativas. A conta está bloqueada.");
+            }
+
         }
 
 
-        System.out.println("****************************");
-        System.out.println("\nDados Iniciais do Cliente:");
-        System.out.println("\nNome: " + nome);
-        System.out.println("Tipo de Conta: " + tipoConta);
-        System.out.println("Saldo Inicial: " + saldoInicial);
-        System.out.println("\n");
+
+
+
+
+
+
+
     }
 }
